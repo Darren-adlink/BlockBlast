059f0ffeecee73480d160d371d4e3bd200744f4c
Darren-adlink <64460353+Darren-adlink@users.noreply.github.com>
2026-01-09 11:44:10 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BlockBlastSolver from './BlockBlastSolver'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BlockBlastSolver />
+  </React.StrictMode>,
+)
